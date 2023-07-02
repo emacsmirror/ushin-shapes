353746fba36d124c5d0a881a61199d93f659dc59
Joseph Turner <joseph@breatheoutbreathe.in>
2023-07-02 03:14:21 -0700

    Chore: Update license boilerplate to match LICENSE

--- ushin-shapes.el
+++ ushin-shapes.el
@@ -12,17 +12,17 @@
 ;; This file is not part of GNU Emacs
 
 ;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; it under the terms of the GNU Affero General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; Affero General Public License for more details.
 
-;; For a full copy of the GNU General Public License
-;; see <https://www.gnu.org/licenses/>.
+;; For a full copy of the GNU Affero General Public License see
+;; <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
